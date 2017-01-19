24b00248362bdb5095b6946b9b8e4f2c7e98a6b7
杨伊博 <527515025@qq.com>
2017-01-19 14:39:40 +0800

    no message

--- springboot-security/src/main/java/com/us/example/domain/SysUser.java
+++ springboot-security/src/main/java/com/us/example/domain/SysUser.java
@@ -1,12 +1,7 @@
 package com.us.example.domain;
 
-import java.util.*;
 
 import javax.persistence.*;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  * Created by yangyibo on 17/1/17.
@@ -14,45 +9,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "Sys_User")
-public class SysUser implements UserDetails { //用户实体实现UserDetails接口,我门的用户实体即位 security 所使用的用户
+public class SysUser { //用户实体实现UserDetails接口,我门的用户实体即位 security 所使用的用户
 
-
-
-    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue
     private Long id;
     private String username;
     private String password;
     private Integer role_id;
-    @Transient //此字段不映射到数据库
-    private String authority;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() { //重写getAuthorities方法,将用户角色作为权限
-        List<GrantedAuthority> auths = new ArrayList<>();
-        auths.add(new SimpleGrantedAuthority(this.authority));
-        return auths;
-    }
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 
     public Long getId() {
         return id;
@@ -85,14 +49,6 @@ public class SysUser implements UserDetails { //用户实体实现UserDetails接
 
     public void setRole_id(Integer role_id) {
         this.role_id = role_id;
-    }
-
-    public String getAuthority() {
-        return authority;
-    }
-
-    public void setAuthority(String authority) {
-        this.authority = authority;
     }
 
 }
--- springboot-security/src/main/java/com/us/example/security/CustomUserService.java
+++ springboot-security/src/main/java/com/us/example/security/CustomUserService.java
@@ -4,10 +4,14 @@ import com.us.example.dao.SysRoleRepositiory;
 import com.us.example.dao.SysUserRepository;
 import com.us.example.domain.SysUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by yangyibo on 17/1/18.
@@ -23,11 +27,17 @@ public class CustomUserService implements UserDetailsService { //1
     public UserDetails loadUserByUsername(String username) { //2
 
         SysUser user = userRepository.findByUsername(username);
-        user.setAuthority(sysRoleRepositiory.findById(user.getRole_id()).getName());
         if(user == null){
             throw new UsernameNotFoundException("用户名不存在");
         }
-        return user; //3
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        //用于添加用户的权限。只要把用户权限添加到authorities 就万事大吉。
+        authorities.add(new SimpleGrantedAuthority(sysRoleRepositiory.findById(user.getRole_id()).getName()));
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),
+                user.getPassword(), authorities);
+
     }
 
 }
