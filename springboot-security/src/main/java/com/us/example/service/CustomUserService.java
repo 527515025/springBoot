e14bc1ccb505f04a8a89c7337a931b6cdcb7cfcc
杨伊博 <527515025@qq.com>
2017-01-20 18:10:33 +0800

    ok

--- springboot-security/src/main/java/com/us/example/service/CustomUserService.java
+++ springboot-security/src/main/java/com/us/example/service/CustomUserService.java
@@ -28,25 +28,6 @@ public class CustomUserService implements UserDetailsService { //自定义UserDe
     @Autowired
     PermissionDao permissionDao;
 
-//    @Override
-//    public UserDetails loadUserByUsername(String username) { //重写loadUserByUsername 方法获得 userdetails 类型用户
-//
-//        SysUser user = userDao.findByUserName(username);
-//        if(user == null){
-//            throw new UsernameNotFoundException("用户名不存在");
-//        }
-//        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
-//        //用于添加用户的权限。只要把用户权限添加到authorities 就万事大吉。
-//        for(SysRole role:user.getRoles())
-//        {
-//            authorities.add(new SimpleGrantedAuthority(role.getName()));
-//            System.out.println(role.getName());
-//        }
-//        return new org.springframework.security.core.userdetails.User(user.getUsername(),
-//                user.getPassword(), authorities);
-//
-//    }
-
     public UserDetails loadUserByUsername(String username) {
         SysUser user = userDao.findByUserName(username);
         if (user != null) {
