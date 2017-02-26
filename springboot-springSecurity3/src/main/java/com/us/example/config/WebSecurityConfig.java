0b7536421b23ee39f096ff1c5f7d9861ca4557cc
杨伊博 <527515025@qq.com>
2017-02-26 22:35:01 +0800

    add @EnableWebSecurity

--- springboot-springSecurity3/src/main/java/com/us/example/config/WebSecurityConfig.java
+++ springboot-springSecurity3/src/main/java/com/us/example/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
@@ -17,6 +18,7 @@ import org.springframework.security.web.access.intercept.FilterSecurityIntercept
 
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
