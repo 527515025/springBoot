0e2aa3167b36622904ae819c831df2b17e005674
杨伊博 <527515025@qq.com>
2019-04-02 15:22:13 +0800

    删除news

--- springboot-mybatis2/src/test/java/cn/abel/service/ServiceTest.java
+++ springboot-mybatis2/src/test/java/cn/abel/service/ServiceTest.java
@@ -1,7 +1,6 @@
 package cn.abel.service;
 
 import cn.abel.BaseTest;
-import cn.abel.bean.News;
 import cn.abel.bean.User;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,16 +13,14 @@ import java.util.List;
  */
 public class ServiceTest extends BaseTest {
 
-    @Autowired
-    private NewsService newsService;
+
     @Autowired
     private UserService userService;
 
     @Test
     public void dynamicDataSourceTest() throws Exception {
-        List<News> newsList = newsService.getByMap(null);
         List<User> userList = userService.getByMap(null);
-        System.out.println("newsList----" + newsList.size() + "--userList---" + userList.size());
+        System.out.println("--userList---" + userList.size());
 
     }
 }
