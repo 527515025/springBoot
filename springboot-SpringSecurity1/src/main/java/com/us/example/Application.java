80670479027d3ef9eb1f75affb7897c3bd3a4ee4
zhangpfg <15991749357@163.com>
2019-09-04 14:47:05 +0800

    修改配置文件，及数据库用户名，方便本地服务正常运行

--- springboot-SpringSecurity1/src/main/java/com/us/example/Application.java
+++ springboot-SpringSecurity1/src/main/java/com/us/example/Application.java
@@ -1,16 +1,17 @@
 package com.us.example;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 import static org.springframework.boot.SpringApplication.run;
 
 /**
- * Created by yangyibo on 17/1/17.
+ * Created by yangyibo on 17/1/17.springBoot+springSecurity 数据库动态管理用户、角色、权限
  */
 
-@ComponentScan(basePackages ="com.us.example")
+@EnableCaching
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
---
+++ springboot/src/main/java/com/atguigu/Test.java
@@ -0,0 +1,7 @@
+package com.atguigu;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("dd");
+    }
+}
